3d85dbcfedadd374085086793d62722f9a97b113
ubairnits <47136841+ubairnits@users.noreply.github.com>
2019-04-19 15:23:42 +0530

    Nits api updated by ubair mir

--- App_Code/NitsAPI.cs
+++ App_Code/NitsAPI.cs
@@ -9,21 +9,39 @@ namespace NITS.API
 {
     public class NitsAPI
     {
-        public static HttpClient apiConnection()
+        public static HttpClient apiConnection1()
         {
 
             //Hosted web API REST Service base url  
            
              string BaseUrl = "http://fwcore.easio.in/api/";
 
-
-           
             HttpClient client = new HttpClient();
             client.BaseAddress = new Uri(BaseUrl);
             client.DefaultRequestHeaders.Clear();
             //Define request data format  
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
            
+            //Sending request to find web api REST service resource GetAllEmployees using HttpClient  
+            // HttpResponseMessage response = client.GetAsync("api/str").Result;
+
+            return client;
+        }
+        public static HttpClient apiConnection()
+        {
+
+            //Hosted web API REST Service base url  
+
+            
+            string BaseUrl = "http://timetable.easio.in/api/";
+
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(BaseUrl);
+            client.DefaultRequestHeaders.Clear();
+            //Define request data format  
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
             //Sending request to find web api REST service resource GetAllEmployees using HttpClient  
             // HttpResponseMessage response = client.GetAsync("api/str").Result;
 
