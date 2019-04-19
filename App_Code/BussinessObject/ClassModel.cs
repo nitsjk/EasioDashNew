9554f5ae715b6aa664e27c08d39ec632764db482
ubairnits <47136841+ubairnits@users.noreply.github.com>
2019-04-19 15:17:41 +0530

    Two Mode Classes Updated by ubair mir

---
+++ App_Code/BussinessObject/ClassModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Model For Classes
+/// </summary>
+namespace Nits.Model
+{
+    public class ClassModel
+    {
+        public long ClassId { get; set; }
+        public long SubDepartmentID { get; set; }
+        public bool SessionID { get; set; }
+        public string EduDepartmentName { get; set; }
+        public string Current_Session { get; set; }
+        public string ClassName { get; set; }
+        public string ClassIncharg { get; set; }
+        public string UserName { get; set; }
+        public string MonthName { get; set; }
+        public bool isDeleted { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime UpdatedOn { get; set; }
+        public string SYear { get; set; }
+
+    }
+}
---
+++ App_Code/BussinessObject/ImranData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for ImranData
+/// </summary>
+namespace Nits.Model
+{
+    public class ImranData
+    {
+        public long ID { set; get; }
+        public string Name { set; get; }
+        public string Address { set; get; }
+        public string Phone { set; get; }
+    }
+}
