97e7b1801517f21292eff1772ae5395620f3a601
ubairnits <47136841+ubairnits@users.noreply.github.com>
2019-04-24 15:02:06 +0530

    updated by ubair mir

---
+++ App_Code/DataAccess/SectionBLL.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nits.Model;
+using System.Data.SqlClient;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Net;
+using NITS.API;
+using System.Data;
+
+/// <summary>
+/// Data Access Leayer from sections 
+/// </summary>
+namespace Nits.BLL
+{
+    public class SectionBLL
+    {
+        public List<Section> GetSections(long Classid)
+        {
+            HttpClient hc = NitsAPI.apiConnection1();
+            HttpResponseMessage response = hc.GetAsync("Section/" + Classid).Result;
+            List<Section> SList = response.Content.ReadAsAsync<IEnumerable<Section>>().Result.ToList();
+            return SList;
+        }
+        public string AddSection(Section model)
+        {
+            HttpClient http = NitsAPI.apiConnection1();
+            var ReturnMessage = http.PostAsJsonAsync("Section/" + model.Classid, model).Result;
+
+            if (ReturnMessage.IsSuccessStatusCode)
+            {
+                return ReturnMessage.Content.ReadAsAsync<string>().Result;
+            }
+            else
+            {
+                return ReturnMessage.Content.ReadAsAsync<string>().Result;
+            }
+        }
+        public string DeleteSection(long SectionID)
+        {
+            HttpClient http = NitsAPI.apiConnection1();
+            var ReturnMessage = http.DeleteAsync("Section/" + SectionID).Result;
+            if (ReturnMessage.IsSuccessStatusCode)
+            {
+                return ReturnMessage.Content.ReadAsAsync<string>().Result;
+            }
+            else
+            {
+                return ReturnMessage.Content.ReadAsAsync<string>().Result;
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// edit section 
+        /// <returns></returns>
+        public Section EditSectin(long Classid)
+        {
+            HttpClient hc = NitsAPI.apiConnection1();
+
+            HttpResponseMessage response = hc.GetAsync("Section/" + Classid).Result;
+            Section SecModel = response.Content.ReadAsAsync<Section>().Result;
+
+            return SecModel;
+        }
+        public string UpdateClass(Section model)
+        {
+            HttpClient http = NitsAPI.apiConnection1();
+            HttpResponseMessage response = http.GetAsync("Section/" + model.Classid.ToString()).Result;
+            List<Section> SList = response.Content.ReadAsAsync<IEnumerable<Section>>().Result.ToList();
+            Section Smodel = SList.FirstOrDefault(x => x.SectionID == model.SectionID);
+            Smodel.ClassName = model.ClassName;
+            Smodel.SectionName = model.SectionName;
+            var ReturnMessage = http.PutAsJsonAsync("Section", Smodel).Result;
+            if (ReturnMessage.IsSuccessStatusCode)
+            {
+                return ReturnMessage.Content.ReadAsAsync<string>().Result;
+            }
+            else
+            {
+                return ReturnMessage.Content.ReadAsAsync<string>().Result;
+            }
+        }
+    }
+}
