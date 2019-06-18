e3ad1a5921d341080d11447baea8e38b58ce167f
imrannits <Admin123###>
2019-06-18 16:29:04 +0530

    upadted by umar

---
+++ App_Code/Models/Unit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Nits.Model
+{
+    public class Unit
+    {
+        public  long unitid { get; set; }
+        public string unitName { get; set; }
+
+    }
+
+}
